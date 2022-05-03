8d6f23da0258b0684b84f9cf11ebfba0aa29454d
MutheuLinet <muneenilinet@gmail.com>
2022-05-03 14:49:30 +0300

    Add Squad object properties, getters and setters

---
+++ src/main/java/models/Squad.java
@@ -0,0 +1,54 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Squad {
+    private String maxSize;
+    private String squadName;
+    private String squadCause;
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Squad(String maxSize, String squadName, String squadCause) {
+        this.maxSize = maxSize;
+        this.squadName = squadName;
+        this.squadCause = squadCause;
+    }
+
+    public String getMaxSize() {
+        return maxSize;
+    }
+
+    public String getSquadName() {
+        return squadName;
+    }
+
+    public String getSquadCause() {
+        return squadCause;
+    }
+
+    public void setMaxSize(String maxSize) {
+        this.maxSize = maxSize;
+    }
+
+    public void setSquadName(String squadName) {
+        this.squadName = squadName;
+    }
+
+    public void setSquadCause(String squadCause) {
+        this.squadCause = squadCause;
+    }
+
+    public void addHero(Hero hero){
+
+
+    }
+}
